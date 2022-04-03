0e9ec414c9f7be93ac5d770b7920f980acf98fa3
Damian2057 <ddamian2057@gmail.com>
2022-04-03 16:01:50 +0200

    Initial implementation of Key blocks

--- src/main/java/crypto/project/Model/crypto/component/KeyBlock.java
+++ src/main/java/crypto/project/Model/crypto/component/KeyBlock.java
@@ -8,6 +8,8 @@ public class KeyBlock {
     private final byte[] desKey;
     private byte[] leftKey;
     private byte[] rightKey;
+    private byte[] PC2 = new byte[48];
+    private byte[] connected = new byte[56];
     private final byte[] leftKeyPattern = {
             57, 49, 41, 33, 25, 17,  9,
             1, 58, 50, 42, 34, 26, 18,
@@ -20,7 +22,7 @@ public class KeyBlock {
             14,  6, 61, 53, 45, 37, 29,
             21, 13,  5, 28, 20, 12,  4
     };
-    private final byte[] PC2 = {
+    private final byte[] PC2Pattern = {
             14, 17, 11, 24,  1,  5,  3,
             28, 15,  6, 21, 10, 23, 19,
             12,  4, 26,  8, 16,  7, 27,
@@ -38,5 +40,62 @@ public class KeyBlock {
         this.desKey = desKey;
         leftKey = PermutationFunction.permute(leftKeyPattern, Converter.toBinaryTab(desKey),28);
         rightKey = PermutationFunction.permute(rightKeyPattern, Converter.toBinaryTab(desKey),28);
+    }
+
+    public void encryptByRound(int round) {
+        left(shiftTable[round]);
+        connectBlocks();
+        permPC2();
+    }
+
+    public void decryptByRound(int round) {
+        right(shiftTable[round]);
+        connectBlocks();
+        permPC2();
+    }
+
+    private void left(byte x) {
+        byte leftTemp = 0;
+        byte rightTemp = 0;
+        for (int i = 0; i < x; i++) {
+            leftTemp = leftKey[0];
+            rightTemp = rightKey[0];
+
+            for (int j = 0; j < 27; j++) {
+                leftKey[j] = leftKey[j+1];
+                rightKey[j] = rightKey[j+1];
+            }
+            leftKey[27] = leftTemp;
+            rightKey[27] = rightTemp;
+        }
+    }
+
+    private void right(byte x) {
+        byte leftTemp = 0;
+        byte rightTemp = 0;
+        for (int i = 0; i < x; i++) {
+            leftTemp = leftKey[27];
+            rightTemp = rightKey[27];
+
+            for (int j = 27; j > 0; j--) {
+                leftKey[j] = leftKey[j-1];
+                rightKey[j] = rightKey[j-1];
+            }
+            leftKey[0] = leftTemp;
+            rightKey[0] = rightTemp;
+        }
+    }
+
+    public byte[] getPC2() {
+        return PC2;
+    }
+
+    private void connectBlocks() {
+        System.arraycopy(leftKey,0, connected, 0,28);
+        System.arraycopy(rightKey, 0, connected, 28, 28);
+    }
+
+    private void permPC2() {
+        PC2 = PermutationFunction.permute(PC2Pattern, connected,48);
     }
 }
